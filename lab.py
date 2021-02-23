d2ea3cb41168771a9f4f8610a66eea3ff7a49aea
pooja rijal <78839540+pooja857@users.noreply.github.com>
2021-02-23 09:37:19 +0545

    Initial commit

---
+++ lab.py
@@ -0,0 +1,21 @@
+num=int(input('Enter the number(1-7):'))
+if num==1:
+    print('it is sunday')
+elif num==2:
+    print('it is monday')
+elif num==3:
+    print('it is Tuesday')
+elif num==4:
+    print('it is wednesday')
+elif num==5:
+    print('it is Thrusday')
+elif num==6:
+    print('it is friday')
+elif num==7:
+    print('it is saturday')
+else:
+    print('it is invalid number')
+
+
+
+
